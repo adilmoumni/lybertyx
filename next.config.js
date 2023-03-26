3848c33f3cb005237648646cc1ddb2ccc3e473c7
adil <moumniabdou.adil@gmail.com>
2023-03-26 21:02:41 +0000

    yodate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+    loader: "custom",
+    loaderFile: "public/image/loading.gif",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
