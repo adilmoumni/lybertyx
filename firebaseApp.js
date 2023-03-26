904e71c7fe997c8c59d5e373fc0752a1feb031cf
adil <moumniabdou.adil@gmail.com>
2023-03-26 21:19:51 +0000

    add firebase requiremetn

---
+++ firebaseApp.js
@@ -0,0 +1,22 @@
+const { https, logger } = require('firebase-functions');
+const { default: next } = require('next');
+
+const nextjsDistDir = require('./next.config.js').distDir;
+
+const nextjsServer = next({
+  dev: false,
+  conf: {
+    distDir: nextjsDistDir,
+  },
+});
+const nextjsHandle = nextjsServer.getRequestHandler();
+
+/**
+ *Note: The `nextServer` here will be the Firebase Cloud Functions name
+ */
+exports.nextServer = https.onRequest((req, res) => {
+  return nextjsServer.prepare().then(() => {
+    logger.info(req.path, req.query);
+    return nextjsHandle(req, res);
+  });
+});
